71b52d923f875667335e248d546e3f7257a1358a
ITMaktabaTulMadinah <asst.it@maktabatulmadinah.com>
2025-12-05 17:52:41 +0500

    Add Google Fonts for improved typography, integrate react-fast-marquee for footer announcements, and enhance UI components with updated styles and layout adjustments.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import books from "./bookData";
 import BookCard from "./BookCard";
+import Marquee from "react-fast-marquee";
 
 function App() {
   const [bookCode, setBookCode] = useState("");
   const [filteredBooks, setFilteredBooks] = useState([]);
   const inputRef = useRef(null);
+  const [hasSearched, setHasSearched] = useState(false);
 
   const handleChange = (e) => {
     setBookCode(e.target.value);
   };
 
   const handleKeyDown = (e) => {
+    setHasSearched(true);
     if (e.key === "Enter") {
       const scannedCode = bookCode.trim();
       if (!scannedCode) return;
@@ -28,78 +31,32 @@ function App() {
 
     setTimeout(() => {
       setBookCode("");
-    }, 3000);
+    }, 500);
   };
 
   return (
     <>
-      <footer className="footer sm:footer-horizontal footer-center  bg-accent-content text-base-content py-2 flex items-center justify-between px-6">
-        <aside>
-          <p>
-            Copyright © {new Date().getFullYear()} - All right reserved by
-            Maktaba Tul Madinah
-          </p>
-        </aside>
-        <h1 className="font-medium">Maktaba-Tul-Madinah</h1>
-      </footer>
       <div className="flex flex-col items-center justify-center mt-16">
         <input
           ref={inputRef}
           type="text"
-          className="w-full max-w-3xl mx-auto text-center text-3xl lg:text-4xl py-6 px-8 rounded-2xl border-4 border-emerald-400 focus:outline-none focus:ring-4 focus:ring-emerald-300 shadow-lg transition-all duration-300 placeholder-gray-400"
-          placeholder="Scan Book Barcode"
+          className="w-full max-w-3xl mx-auto text-center text-3xl lg:text-4xl py-6 px-8 border-none focus:outline-none"
+          // placeholder="Scan Book Barcode"
           value={bookCode}
           onChange={handleChange}
           onKeyDown={handleKeyDown}
         />
 
-        {/* <input
-          ref={inputRef}
-          type="text"
-          className="input text-center text-3xl w-2xl py-8"
-          placeholder="Scan Book Barcode"
-          value={bookCode}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-        /> */}
-
         <div>
-          {/* {filteredBooks.length === 0 && (
-            <div className="hero my-20">
-              <div className="hero-content flex-col items-center justify-center">
-                <h1 className="text-5xl font-bold">No Book Found</h1>
-                <p className="text-2xl">Please contact the counter.</p>
-              </div>
+          {!hasSearched && (
+            <div className="max-w-lg mx-auto my-24 bg-white rounded-3xl shadow-lg p-12 flex flex-col items-center justify-center transition-transform transform hover:scale-105 duration-300">
+              <img
+                src="https://maktabatulmadinah.com/storage/frontendsetting/1758110585136789.webp"
+                alt=""
+              />
             </div>
-          )} */}
-          {/* {filteredBooks.length > 0 &&
-            filteredBooks.map((data) => (
-              <div className="hero my-20" key={data.newCode}>
-                <div className="hero-content flex-col lg:flex-row">
-                  <figure>
-                    {data.newCode && (
-                      <img
-                        src={`https://maktabatulmadinah.com/Images/${`0${data.newCode}`}/01.webp`}
-                        className="max-w-120 rounded-lg"
-                        alt="Book"
-                      />
-                    )}
-                  </figure>
-                  <div className="flex flex-col gap-4">
-                    <h1 className="text-5xl font-bold">
-                      {data.bookNameInEnglish}
-                    </h1>
-                    <h1 className="text-5xl font-bold mb-6 text-right">
-                      {data.bookNameInUrdu}
-                    </h1>
-                    <h4 className="text-emerald-400 text-6xl font-bold">
-                      Rs. {data.priceAfterDiscount}/-
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            ))} */}
-          {filteredBooks.length === 0 && (
+          )}
+          {hasSearched && filteredBooks.length === 0 && (
             <div className="max-w-lg mx-auto my-24 bg-white rounded-3xl shadow-lg p-12 flex flex-col items-center justify-center transition-transform transform hover:scale-105 duration-300">
               <h1 className="text-5xl font-extrabold text-gray-900 mb-6 text-center">
                 No Book Found
@@ -116,6 +73,15 @@ function App() {
             ))}
         </div>
       </div>
+      <footer className="footer sm:footer-horizontal footer-center  bg-accent-content text-base-content py-2 h-16 flex items-center justify-between px-6 absolute bottom-0">
+        <Marquee
+          speed={100}
+          pauseOnHover={true}
+          className="text-white font-medium text-2xl mr- block"
+        >
+          {`Welcome to Maktaba-Tul-Madinah KWBF (Karachi World Book Fair) 2025 Stall No 21, 22, 23`}
+        </Marquee>
+      </footer>
     </>
   );
 }
--- src/BookCard.jsx
+++ src/BookCard.jsx
@@ -5,7 +5,7 @@ const BookCard = ({ data }) => {
   const [loading, setLoading] = useState(true);
 
   return (
-    <div className="max-w-7xl mx-auto my-10 h-[500px] bg-white border border-gray-200 rounded-3xl shadow-lg hover:shadow-2xl transition-shadow duration-300 overflow-hidden">
+    <div className="max-w-7xl md:w-6xl  mx-auto my-10 h-[500px] bg-white border border-gray-200 rounded-3xl shadow-lg hover:shadow-2xl transition-shadow duration-300 overflow-hidden">
       <div className="lg:flex h-full">
         {/* Image Section */}
         <div className="relative lg:w-1/3 h-full flex justify-center items-center bg-gray-100 group overflow-hidden">
@@ -28,19 +28,23 @@ const BookCard = ({ data }) => {
         </div>
 
         {/* Details Section */}
-        <div className="lg:w-2/3 p-8 flex flex-col justify-between">
+        <div className="lg:w-2/3 py-8 px-0 flex flex-col justify-between">
           <div>
             <h1 className="text-4xl lg:text-5xl text-center font-extrabold text-gray-900 mb-2">
               {data.productNameEnglish.toUpperCase()}
             </h1>
-            <h2 className="text-3xl lg:text-4xl text-gray-600 text-center pt-6">
+            <h2 className="text-3xl lg:text-4xl text-gray-600 font-bold text-center pt-6">
               {data.productNameUrdu}
             </h2>
           </div>
 
           <div className="mt-6 flex items-center justify-center">
-            <span className="text-3xl lg:text-9xl font-bold text-emerald-500 px-4 py-2 rounded-xl ">
-              Rs. {data.retailAfterDiscount - data.retailAfterDiscount * 0.2} /-
+            <span className="text-3xl lg:text-9xl font-bold text-black px-4 py-2 rounded-xl">
+              Rs.{" "}
+              {Math.round(
+                data.retailAfterDiscount - data.retailAfterDiscount * 0.2
+              )}{" "}
+              /-
             </span>
           </div>
         </div>
